c6cacf703e0333b231a18e6dcc8841f2acd9909b
vladislav-garaev <marker661@gmail.com>
2023-08-08 15:34:39 +0300

    feat: implement password visibility btn

--- src/components/forms/inputs/PasswordInput.tsx
+++ src/components/forms/inputs/PasswordInput.tsx
@@ -1,6 +1,15 @@
 import { Field, ErrorMessage } from 'formik';
+import view from '../../../assets/view.svg';
+import noView from '../../../assets/no-view.svg';
+import { useState } from 'react';
 
 export const PasswordInput = () => {
+  const [passwordVisibility, setPasswordVisibility] = useState(false);
+
+  const handleVisibility = () => {
+    setPasswordVisibility((prev: boolean) => !prev);
+  };
+
   return (
     <>
       <label
@@ -9,12 +18,23 @@ export const PasswordInput = () => {
       >
         Password
       </label>
-      <Field
-        className="focus:shadow-outline mb-3 appearance-none rounded border border-red-500 px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
-        type="password"
-        name="password"
-        placeholder="Password"
-      />
+      <div className="relative">
+        <Field
+          className="focus:shadow-outline mb-3 w-full appearance-none rounded border border-red-500 px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
+          type={passwordVisibility ? 'text' : 'password'}
+          name="password"
+          placeholder="Password"
+        />
+        <button
+          onClick={handleVisibility}
+          className="absolute right-2 top-2 w-5"
+        >
+          <img
+            src={passwordVisibility ? view : noView}
+            alt="visibility icon"
+          />
+        </button>
+      </div>
       <ErrorMessage
         name="password"
         component="p"
