f3d13ae5727601ea23411dc1061c29e34cbc46ce
Maxim <fil.maxim13@gmail.com>
2023-08-10 23:10:10 +0300

    fix: add : to all inputs label

--- src/components/forms/inputs/FirstNameInput.tsx
+++ src/components/forms/inputs/FirstNameInput.tsx
@@ -7,7 +7,7 @@ export const FirstNameInput = () => {
         htmlFor="firstName"
         className="text-sm font-bold text-gray-700"
       >
-        First Name
+        First Name:
       </label>
       <Field
         className="focus:shadow-outline appearance-none rounded border border-cyan-500 px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
