3554ecf9bb8791881901deec38395803dd85598f
Pavel <p.yudenko@gmail.com>
2023-08-10 23:58:11 +0300

    feat: implement saving tokens to localStorage

--- src/components/authProvider/utils.ts
+++ src/components/authProvider/utils.ts
@@ -1,6 +1,5 @@
 import type { UserDataInterface } from '../../utils/types';
 
 export const login = (userData: UserDataInterface): void => {
-  // localStorage.setItem('userData', JSON.stringify(userData));
-  console.log('auth is succes!', userData);
+  localStorage.setItem('userData', JSON.stringify(userData));
 };
