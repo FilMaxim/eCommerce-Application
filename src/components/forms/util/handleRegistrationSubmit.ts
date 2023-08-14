69e946cf7a931586ee94746bc0fcf91d4cf5fd13
vladislav-garaev <marker661@gmail.com>
2023-08-14 11:54:08 +0300

    refactor: switch registration into sdk

--- src/components/forms/util/handleRegistrationSubmit.ts
+++ src/components/forms/util/handleRegistrationSubmit.ts
@@ -1,27 +1,34 @@
-import { createCustomer } from '../../../helpers/api/createCustomer';
-import { requestAnonymousToken } from '../../../helpers/api/requestAnonymousToken';
+import { createCustomer } from '../../../helpers/api/apiRoot';
 import { showToastMessage } from '../../../helpers/showToastMessage';
 import type { HandleSubmitInterface, LoginInterface } from '../../../utils/types';
 import { addressAdapter } from './addressDataAdapter';
+
+const SUCCESSFUL_REGISTRATION_MESSAGE = 'Registration successful';
+const EXISTING_CUSTOMER_ERROR = 'There is already an existing customer with the provided email.';
+const EXISTING_CUSTOMER_MESSAGE =
+  'Customer with this email already exist, login or create new account';
+const OTHER_ERROR_MESSAGE = 'Registration failed, please try again later';
 
 export const handleRegistrationSubmit = async (
   values: HandleSubmitInterface,
   login: (values: LoginInterface) => Promise<void>
 ): Promise<void> => {
   const normalizedData = addressAdapter(values);
-  const accessToken = (await requestAnonymousToken()).accessToken;
-  const response = await createCustomer(normalizedData, accessToken);
-  const data = await response.json();
+  try {
+    const response = await createCustomer(normalizedData);
 
-  if (response.ok) {
-    await login({
-      email: values.email,
-      password: values.password
-    });
-    showToastMessage('Registration successful', 'green');
-    return;
+    if (response.statusCode === 201) {
+      // await login({
+      //   email: values.email,
+      //   password: values.password
+      // });
+      showToastMessage(SUCCESSFUL_REGISTRATION_MESSAGE, 'green');
+    }
+  } catch (error) {
+    if (!(error instanceof Error)) return;
+
+    error.message === EXISTING_CUSTOMER_ERROR
+      ? showToastMessage(EXISTING_CUSTOMER_MESSAGE, 'red')
+      : showToastMessage(OTHER_ERROR_MESSAGE, 'red');
   }
-  data.errors[0].code === 'DuplicateField'
-    ? showToastMessage('Customer with this email already exist, login or create new account', 'red')
-    : showToastMessage('Registration failed, please try again later', 'red');
 };
--- src/helpers/api/apiRoot.ts
+++ src/helpers/api/apiRoot.ts
@@ -1,4 +1,5 @@
 import { clientApiData } from '../../utils/clientApiData';
+import type { CustomerData } from '../../utils/types';
 import { ctpClient } from './BuildClient';
 import {
   // ApiRoot,
@@ -6,14 +7,22 @@ import {
 } from '@commercetools/platform-sdk';
 
 // Create apiRoot from the imported ClientBuilder and include your Project key
-const apiRoot = createApiBuilderFromCtpClient(ctpClient)
-  .withProjectKey({ projectKey: clientApiData.projectKey });
+const apiRoot = createApiBuilderFromCtpClient(ctpClient).withProjectKey({
+  projectKey: clientApiData.projectKey
+});
 
 // Example call to return Project information
 // This code has the same effect as sending a GET request to the commercetools Composable Commerce API without any endpoints.
 export const getCustomers = async () => {
+  return await apiRoot.customers().get().execute();
+};
+
+export const createCustomer = async (data: CustomerData) => {
   return await apiRoot
     .customers()
-    .get()
+    .post({
+      // The CustomerDraft is the object within the body
+      body: data
+    })
     .execute();
 };
