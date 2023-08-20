7b76cbf39fd26d3b4a703063fd726b2cf6213c30
vladislav-garaev <marker661@gmail.com>
2023-08-20 19:12:01 +0300

    feat: implement registration form render tests (snapshot & checkbox expanding)

---
+++ src/__tests__/registrationFormRender.test.tsx
@@ -0,0 +1,55 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+import { RegistrationForm } from '../components/forms/RegistrationForm/RegistrationForm';
+import { initialValues } from '../components/forms/inputs/inputsData';
+import { getValidationSchema } from '../components/forms/util/validationSchema';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+describe('RegistrationForm renders correctly', () => {
+  it('renders correctly', () => {
+    const component = renderer.create(
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path={''}
+            element={
+              <RegistrationForm
+                initialValues={initialValues}
+                onSubmit={jest.fn()}
+                getValidationSchema={getValidationSchema}
+              />
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    );
+
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('expands when billing address checkbox is unchecked', () => {
+    render(
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path={''}
+            element={
+              <RegistrationForm
+                initialValues={initialValues}
+                onSubmit={jest.fn()}
+                getValidationSchema={getValidationSchema}
+              />
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    );
+
+    expect(screen.queryByText(/Billing address/i)).toBeFalsy();
+
+    fireEvent.click(screen.getByLabelText(/to use the same address for both billing and shipping/i));
+
+    expect(screen.getByText(/Billing address/i)).toBeTruthy();
+  });
+});
