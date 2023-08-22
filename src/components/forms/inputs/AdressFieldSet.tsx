e231429682d46eb28cb2e8c095e0aded2478e1a6
Pavel <p.yudenko@gmail.com>
2023-08-22 23:45:21 +0300

    refactor: move CountryInput to separate module

--- src/components/forms/inputs/AdressFieldSet.tsx
+++ src/components/forms/inputs/AdressFieldSet.tsx
@@ -1,49 +1,10 @@
-import { Field, ErrorMessage } from 'formik';
-import { countries } from '../util/countriesList';
 import { Input } from './Input';
 import { FieldSetName, inputsData } from './inputsData';
 import type { AddressFieldSetProps } from '../../../utils/types';
 import { PostalcodeInput } from './PostalcodeInput';
-
-const Country = ({ fieldSet }: AddressFieldSetProps) => {
-  return (
-    <>
-      <label
-        htmlFor={`${fieldSet}Country`}
-        className="text-sm font-bold text-gray-700"
-      >
-        Country:
-      </label>
-      <Field
-        as="select"
-        className="focus:shadow-outline appearance-none rounded border border-cyan-500 px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
-        type="text"
-        name={`${fieldSet}Country`}
-        placeholder="Country"
-      >
-        <option
-          value=""
-          disabled
-        >
-          Select a country
-        </option>
-        {countries.map(({ country }) => (
-          <option
-            key={country}
-            value={country}
-          >
-            {country}
-          </option>
-        ))}
-      </Field>
-      <ErrorMessage
-        name={`${fieldSet}Country`}
-        component="p"
-        className="text-xs italic text-red-500"
-      />
-    </>
-  );
-};
+import { CountryInput } from './CountryInput';
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
 
 export const AdressFieldSet = ({ fieldSet, formik }: AddressFieldSetProps) => {
   const { streetName, city, postalCode } = inputsData.addressFieldSet;
@@ -65,7 +26,7 @@ export const AdressFieldSet = ({ fieldSet, formik }: AddressFieldSetProps) => {
         placeholder={city.placeholder}
         formik={formik}
       />
-      <Country
+      <CountryInput
         fieldSet={fieldSet}
         formik={formik}
       />
@@ -76,16 +37,16 @@ export const AdressFieldSet = ({ fieldSet, formik }: AddressFieldSetProps) => {
         formik={formik}
         fieldSet={fieldSet}
       />
-      <div className="mb-[0.125rem] ml-auto block min-h-[1.5rem] pl-[1.5rem]">
-        <label className="inline-block pl-[0.15rem] hover:cursor-pointer">
-          <Field
-            className="checked:border-primary checked:bg-primary dark:checked:border-primary dark:checked:bg-primary relative float-left -ml-[1.5rem] mr-[6px] mt-[0.15rem] h-[1.125rem] w-[1.125rem] appearance-none rounded-[0.25rem] border-[0.125rem] border-solid border-neutral-300 outline-none before:pointer-events-none before:absolute before:h-[0.875rem] before:w-[0.875rem] before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] checked:before:opacity-[0.16] checked:after:absolute checked:after:-mt-px checked:after:ml-[0.25rem] checked:after:block checked:after:h-[0.8125rem] checked:after:w-[0.375rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-l-0 checked:after:border-t-0 checked:after:border-solid checked:after:border-white checked:after:bg-transparent checked:after:content-[''] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-[0.875rem] focus:after:w-[0.875rem] focus:after:rounded-[0.125rem] focus:after:content-[''] checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:after:-mt-px checked:focus:after:ml-[0.25rem] checked:focus:after:h-[0.8125rem] checked:focus:after:w-[0.375rem] checked:focus:after:rotate-45 checked:focus:after:rounded-none checked:focus:after:border-[0.125rem] checked:focus:after:border-l-0 checked:focus:after:border-t-0 checked:focus:after:border-solid checked:focus:after:border-white checked:focus:after:bg-transparent dark:border-neutral-600 dark:focus:before:shadow-[0px_0px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca]"
-            type="checkbox"
+      <FormControlLabel
+        control={
+          <Checkbox
             name={`${fieldSet}StateChecked`}
+            defaultChecked
           />
-          as default
-        </label>
-      </div>
+        }
+        label="as default"
+        labelPlacement="start"
+      />
     </div>
   );
 };
---
+++ src/components/forms/inputs/CountryInput.tsx
@@ -0,0 +1,43 @@
+import { ErrorMessage, Field } from 'formik';
+import type { AddressFieldSetProps } from '../../../utils/types';
+import { countries } from '../util/countriesList';
+
+export const CountryInput = ({ fieldSet }: AddressFieldSetProps) => {
+  return (
+    <>
+      <label
+        htmlFor={`${fieldSet}Country`}
+        className="text-sm font-bold text-gray-700"
+      >
+        Country:
+      </label>
+      <Field
+        as="select"
+        className="focus:shadow-outline appearance-none rounded border border-cyan-500 px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
+        type="text"
+        name={`${fieldSet}Country`}
+        placeholder="Country"
+      >
+        <option
+          value=""
+          disabled
+        >
+          Select a country
+        </option>
+        {countries.map(({ country }) => (
+          <option
+            key={country}
+            value={country}
+          >
+            {country}
+          </option>
+        ))}
+      </Field>
+      <ErrorMessage
+        name={`${fieldSet}Country`}
+        component="p"
+        className="text-xs italic text-red-500"
+      />
+    </>
+  );
+};
