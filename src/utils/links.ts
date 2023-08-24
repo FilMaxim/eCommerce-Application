05a32eec58002d142d420128c8007ae1b9dc81fe
vladislav-garaev <marker661@gmail.com>
2023-08-24 17:08:30 +0300

    refactor: prettier links

--- src/utils/links.ts
+++ src/utils/links.ts
@@ -5,12 +5,33 @@ import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
 import LoginOutlinedIcon from '@mui/icons-material/LoginOutlined';
 
 export const links = {
-  main: { text: 'Home', path: NavRoutes.mainPagePath },
-  about: { text: 'About', path: NavRoutes.aboutPage },
-  contacts: { text: 'Contacts', path: NavRoutes.contactsPage },
-  login: { text: 'LogIn', path: NavRoutes.loginPagePath, icon: LoginOutlinedIcon },
-  logout: { text: 'Logout', path: NavRoutes.mainPagePath, icon: LogoutOutlinedIcon },
-  profile: { text: 'Profile', path: NavRoutes.profilePagePath, icon: PermIdentityOutlinedIcon },
+  main: {
+    text: 'Home',
+    path: NavRoutes.mainPagePath
+  },
+  about: {
+    text: 'About',
+    path: NavRoutes.aboutPage
+  },
+  contacts: {
+    text: 'Contacts',
+    path: NavRoutes.contactsPage
+  },
+  login: {
+    text: 'LogIn',
+    path: NavRoutes.loginPagePath,
+    icon: LoginOutlinedIcon
+  },
+  logout: {
+    text: 'Logout',
+    path: NavRoutes.mainPagePath,
+    icon: LogoutOutlinedIcon
+  },
+  profile: {
+    text: 'Profile',
+    path: NavRoutes.profilePagePath,
+    icon: PermIdentityOutlinedIcon
+  },
   registration: {
     text: 'Registration',
     path: NavRoutes.registrationPagePath,
