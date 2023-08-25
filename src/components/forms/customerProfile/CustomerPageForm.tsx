21eb4c74c0def791900ca6c8adc4aefe0021275d
vladislav-garaev <marker661@gmail.com>
2023-08-25 15:23:50 +0300

    feat: implement reset form by cancel btn

--- src/components/forms/customerProfile/CustomerPageForm.tsx
+++ src/components/forms/customerProfile/CustomerPageForm.tsx
@@ -5,7 +5,12 @@ import IconButton from '@mui/material/IconButton';
 import type { CustomerPageFormProps } from '../../../utils/types';
 import { Button } from '@mui/material';
 
-export const CustomerPageForm = ({ initialValues, formInner, onSubmit, validationSchema }: CustomerPageFormProps) => {
+export const CustomerPageForm = ({
+  initialValues,
+  formInner,
+  onSubmit,
+  validationSchema
+}: CustomerPageFormProps) => {
   const [editable, setEditable] = useState<boolean>(false);
 
   return (
@@ -14,38 +19,46 @@ export const CustomerPageForm = ({ initialValues, formInner, onSubmit, validatio
       onSubmit={onSubmit}
       validationSchema={validationSchema}
     >
-      <div className="relative pb-3 pt-[3rem]">
-        <Form className=" flex max-w-[42rem] flex-wrap items-center justify-center gap-2">
-          <div className="absolute right-2 top-2">
-            <IconButton
-              aria-label="edit"
-              color="primary"
-              onClick={() => {
-                setEditable(true);
-              }}
-              disabled={editable}
-            >
-              <EditOutlinedIcon />
-            </IconButton>
-          </div>
-          {formInner(editable)}
-          {editable && (
-            <div className="flex w-full justify-center gap-2">
-              {['Cancel', 'Save'].map((btnText) => (
-                <Button
-                  key={btnText}
-                  onClick={() => {
-                    setEditable(false);
-                  }}
-                  color={btnText === 'Cancel' ? 'secondary' : 'primary'}
-                >
-                  {btnText}
-                </Button>
-              ))}
+      {(formik) => (
+        <div className="relative pb-3 pt-[3rem]">
+          <Form className=" flex max-w-[42rem] flex-wrap items-center justify-center gap-2">
+            <div className="absolute right-2 top-2">
+              <IconButton
+                aria-label="edit"
+                color="primary"
+                onClick={() => {
+                  setEditable(true);
+                }}
+                disabled={editable}
+              >
+                <EditOutlinedIcon />
+              </IconButton>
             </div>
-          )}
-        </Form>
-      </div>
+            {formInner(editable)}
+            {editable && (
+              <div className="flex w-full justify-center gap-2">
+                {['Cancel', 'Save'].map((btnText) => (
+                  <Button
+                    key={btnText}
+                    type={btnText === 'Cancel' ? 'button' : 'submit'}
+                    {...(btnText === 'Cancel'
+                      ? {
+                          onClick: () => {
+                            formik.resetForm();
+                            setEditable(false);
+                          }
+                        }
+                      : {})}
+                    color={btnText === 'Cancel' ? 'secondary' : 'primary'}
+                  >
+                    {btnText}
+                  </Button>
+                ))}
+              </div>
+            )}
+          </Form>
+        </div>
+      )}
     </Formik>
   );
 };
