db96c37ef1ccb447a347cbde21cb541ad424d43f
vladislav-garaev <marker661@gmail.com>
2023-08-30 15:52:25 +0300

    fix: getting customer & isLogged from store

--- src/components/AnonymOutlet.tsx
+++ src/components/AnonymOutlet.tsx
@@ -4,7 +4,7 @@ import type { OutletProps, RootState } from '../utils/types';
 import { useSelector } from 'react-redux';
 
 export const AnonymOutlet = ({ children }: OutletProps) => {
-  const isLogged = useSelector((state: RootState) => state.isLogged);
+  const isLogged = useSelector((state: { authData: RootState }) => state.authData.isLogged);
 
   return isLogged ? children : <Navigate to={NavRoutes.loginPagePath} />;
 };
