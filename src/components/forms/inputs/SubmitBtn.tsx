ceee38519965d3978aa97a135852083960820149
vladislav-garaev <marker661@gmail.com>
2023-08-30 16:46:32 +0300

    fix: submit colors

--- src/components/forms/inputs/SubmitBtn.tsx
+++ src/components/forms/inputs/SubmitBtn.tsx
@@ -7,7 +7,7 @@ export const SubmitBtn = ({ title }: SubmitBtnProps) => {
     <>
       <button
         type="submit"
-        className="mt-3 w-[16rem] self-center rounded bg-red-600 px-4 py-2 font-bold text-white transition-all hover:bg-red-700"
+        className="mt-3 w-[16rem] self-center rounded bg-secondary px-4 py-2 font-bold text-white transition-all hover:bg-red-600"
       >
         {title}
       </button>
