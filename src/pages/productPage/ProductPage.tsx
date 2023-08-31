5cc89499b3e39357d454171344d3f7839e7c3740
vladislav-garaev <marker661@gmail.com>
2023-08-31 12:09:33 +0300

    refactor: rendering conditions

--- src/pages/productPage/ProductPage.tsx
+++ src/pages/productPage/ProductPage.tsx
@@ -32,59 +32,57 @@ export const Product = () => {
     }
   }, [id]);
 
+  if (product === null) return <div className="text-center text-2xl">Can&apos;t find product</div>;
+
   return (
     <div className={style.product}>
-      <div className={style.product__slider}>
-        {product !== null && (
-          <Carousel
-            className={style.slider}
-            selectedItem={currentImageIndex}
-            onChange={setCurrentImageIndex}
-            infiniteLoop={true}
-            showArrows={true}
-            showIndicators={false}
-            showStatus={false}
-            interval={5000}
-            autoPlay={true}
-            stopOnHover
+      <Carousel
+        selectedItem={currentImageIndex}
+        onChange={setCurrentImageIndex}
+        infiniteLoop={true}
+        showArrows={true}
+        showIndicators={false}
+        showStatus={false}
+        interval={5000}
+        autoPlay={true}
+        stopOnHover
+      >
+        {product.masterVariant.images?.map((image) => (
+          <div
+            key={id}
+            onClick={() => {
+              setModalPreviewOpen(true);
+            }}
           >
-            {product.masterVariant.images?.map((image) => (
-              <div
-                className={style.foto}
-                key={id}
-                onClick={() => {
-                  setModalPreviewOpen(true);
-                }}
-              >
-                <img src={image.url} alt={image.label} />
-              </div>
-            ))}
-          </Carousel>
-        )}
-        {product === null && <div>Нет доступных изображений</div>}
-      </div>
-      {product?.description != null && product.masterVariant.prices !== undefined && (
-        <div className={style.product__card}>
-          <h1 className={style.product__title}>{product.name['en-US']}</h1>
-          <p className={style.product__desc}>{product.description['en-US']}</p>
+            <img
+              className="max-h-[20rem] object-contain"
+              src={image.url}
+              alt={image.label}
+            />
+          </div>
+        ))}
+      </Carousel>
+      <div className={style.product__card}>
+        <h1 className={style.product__title}>{product.name['en-US']}</h1>
+        <p className={style.product__desc}>{product.description?.['en-US']}</p>
+        {product.masterVariant.prices !== undefined && (
           <PriceTag
             price={product.masterVariant.prices[0].value.centAmount / 100}
             discount={Number(product.masterVariant.prices[0].discounted?.value?.centAmount) / 100}
           />
-          <BlackButton
-            size="medium"
-            variant="contained"
-            sx={{
-              padding: 0,
-              height: 41,
-              width: 270
-            }}
-          >
-            Buy Now
-          </BlackButton>
-        </div>
-      )}
-
+        )}
+        <BlackButton
+          size="medium"
+          variant="contained"
+          sx={{
+            padding: 0,
+            height: 41,
+            width: 270
+          }}
+        >
+          Buy Now
+        </BlackButton>
+      </div>
       <Modal
         className={style.modal}
         isOpen={modalPreviewOpen}
@@ -111,19 +109,20 @@ export const Product = () => {
           showIndicators={false}
           showStatus={false}
         >
-          {product?.masterVariant.images?.map(
-            (foto: { url: string | undefined }, index: React.Key | null | undefined) => (
-              <div
-                className={style.foto}
-                key={index}
-                onClick={() => {
-                  setModalPreviewOpen(true);
-                }}
-              >
-                <img src={foto.url} />
-              </div>
-            )
-          )}
+          {product.masterVariant.images?.map((img, index: React.Key | null | undefined) => (
+            <div
+              className="max-w-[50%] object-contain"
+              key={index}
+              onClick={() => {
+                setModalPreviewOpen(false);
+              }}
+            >
+              <img
+                src={img.url}
+                alt={img.label}
+              />
+            </div>
+          ))}
         </Carousel>
       </Modal>
     </div>
