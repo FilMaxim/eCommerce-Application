d82506a392700a24a9fd390c4a72497aeec747a1
Pavel Yudenka <66093034+Pavelvl21@users.noreply.github.com>
2023-09-08 17:41:44 +0300

    fix: fix rating filter (#73)

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -27,11 +27,10 @@ export const en = {
       },
       rating: {
         title: 'Rating',
-        1: '1 & Up',
-        2: '2 & Up',
-        3: '3 & Up',
-        4: '4 & Up',
-        5: '5 & Up'
+        2: '1 & Up',
+        3: '2 & Up',
+        4: '3 & Up',
+        5: '4 & Up'
       },
       type: {
         title: 'Type',
