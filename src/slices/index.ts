8ca54ae21d2967b554ae49deb438b87208e90786
vladislav-garaev <marker661@gmail.com>
2023-09-10 09:14:27 +0300

    fix: import path register

--- src/slices/index.ts
+++ src/slices/index.ts
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { authReducer } from './authSlice';
-import { productCardsReducer } from './productCardsSlice';
+import { productCardsReducer } from './ProductCardsSlice';
 import { categoriesReducer } from './categoriesSlice';
 
 export const store = configureStore({
