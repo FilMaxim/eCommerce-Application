94bce129484f206fd04cbf6a34c40c9a2e822ca3
Pavel Yudenka <p.yudenko@gmail.com>
2023-09-18 01:35:25 +0300

    chore: update styles

--- src/pages/catalogPage/CatalogContent.tsx
+++ src/pages/catalogPage/CatalogContent.tsx
@@ -58,13 +58,9 @@ export const CatalogContent = () => {
     }
   }, [dispatch, categories, categoryFilter]);
 
-  const memoCurrentFilter = useMemo(
-    () => currentFilter, [currentFilter]
-  );
+  const memoCurrentFilter = useMemo(() => currentFilter, [currentFilter]);
 
-  const memoCategoryFilter = useMemo(
-    () => categoryFilter, [categoryFilter]
-  );
+  const memoCategoryFilter = useMemo(() => categoryFilter, [categoryFilter]);
 
   useEffect(() => {
     const updateData = async () => {
@@ -93,7 +89,7 @@ export const CatalogContent = () => {
     isUpdated && (
       <div className="grid-row-3 grid w-full grid-cols-1 gap-4 sm:col-start-2 sm:row-start-3">
         <div className="flex justify-center">
-          <div className="grid grid-flow-row grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          <div className="grid grid-rows-[508px_auto_auto] grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
             {cardsData.map((item) => {
               const { url, name, description, priceTag, id, attributes } = item;
               const rating = attributes?.find((obj) => obj.name === 'rating');
